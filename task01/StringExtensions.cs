13ddd60476e9847b23a0d49362fd873d8055017a
michaelvarn2006 <mishavarnavskiysek123@gmail.com>
2025-06-29 23:28:28 +0600

    feat: string extensions

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        input = input.ToLower();
+        char[] array = input.ToCharArray();
+        var cleaned = from ch in array
+                      where !(Char.IsPunctuation(ch) || Char.IsWhiteSpace(ch))
+                      select ch;
+        char[] cleanedarr = cleaned.ToArray();
+        char[] reversed = (char[])cleanedarr.Clone();
+        Array.Reverse(reversed);
+        return new string(reversed) == new string(cleanedarr) && cleanedarr.Length > 0;
+    }
+}
