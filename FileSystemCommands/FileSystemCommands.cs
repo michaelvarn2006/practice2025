5a97ba9d1453c3c42abc376488432b009d7c03eb
michaelvarn2006 <mishavarnavskiysek123@gmail.com>
2025-07-03 23:03:09 +0500

    feat: dynamic library

---
+++ FileSystemCommands/FileSystemCommands.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using CommandLib;
+namespace FileSystemCommands;
+
+public class DirectorySizeCommand : ICommand
+{
+    string? DirectoryName { get; set; }
+    public uint Size{ get; private set; }
+    public DirectorySizeCommand(string? DirectoryName)
+    {
+        this.DirectoryName = DirectoryName;
+    }
+    public void Execute()
+    {
+        
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    string? DirectoryName { get; set; }
+    string? Mask { get; set; }
+    public uint Size { get; private set; }
+    public FindFilesCommand(string? DirectoryName, string? Mask)
+    {
+        this.DirectoryName = DirectoryName;
+        this.Mask = Mask;
+    }
+    public void Execute()
+    {
+
+    }
+}
+
---
+++ FileSystemCommandsTests/FileSystemCommandsTests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+using FileSystemCommands;
+namespace FileSystemCommandsTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void DirectorySizeCommand_ShouldCalculateSize()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+
+        var command = new DirectorySizeCommand(testDir);
+        command.Execute(); // Проверяем, что не возникает исключений
+
+        Directory.Delete(testDir, true);
+    }
+
+    [Fact]
+    public void FindFilesCommand_ShouldFindMatchingFiles()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+        File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+
+        var command = new FindFilesCommand(testDir, "*.txt");
+        command.Execute(); // Должен найти 1 файл
+
+        Directory.Delete(testDir, true);
+    }
+}
