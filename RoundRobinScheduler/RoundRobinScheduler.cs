f4da7ffdd45ac5b00efc25fa9373d52a33d4117b
michaelvarn2006 <mishavarnavskiysek123@gmail.com>
2025-07-18 23:28:26 +0600

    feat: command scheduler

---
+++ RoundRobinScheduler/RoundRobinScheduler.cs
@@ -0,0 +1,25 @@
+﻿namespace RoundRobinScheduler;
+
+using ICommand;
+using ISteppable;
+
+public class RoundRobinScheduler : IScheduler.IScheduler
+{
+    private readonly Queue<ICommand> _commands = new();
+    public bool HasCommand() => _commands.Count > 0;
+    public ICommand? Select()
+    {
+        int count = _commands.Count;
+        while (count-- > 0)
+        {
+            var cmd = _commands.Dequeue();
+            if (cmd is ISteppable steppable && steppable.IsDone)
+                continue;
+            _commands.Enqueue(cmd);
+            return cmd;
+        }
+        return null;
+    }
+    public void Add(ICommand cmd) => _commands.Enqueue(cmd);
+}
+
